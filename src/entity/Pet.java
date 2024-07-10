c8cc682e3cc4b7cf4e991c7c37d6521d8cd0fb0f
Joyce12345678 <73724033+Joyce12345678@users.noreply.github.com>
2024-07-10 13:27:22 -0400

    Pet.java

--- src/entity/Pet.java
+++ src/entity/Pet.java
@@ -1,4 +1,56 @@
 package entity;
 
-public class Pet {
+import java.util.ArrayList;
+
+public class Pet{
+    private int petID;
+    private String species;
+    private int age;
+    private String breed;
+    private ArrayList personality;
+    private String bio;
+    private boolean isAvailable;
+
+    public Pet(int petID, String species, int age, String breed, ArrayList personality, String bio, boolean isAvailable){
+        this.petID = petID;
+        this.species = species;
+        this.age = age;
+        this.breed = breed;
+        this.personality = personality;
+        this.bio = bio;
+        this.isAvailable = true;
+    }
+    public String getSpecies(){
+        return species;
+    }
+
+    public int getPetAge(){
+        return age;
+    }
+    public void petGetsOlder(){
+        age +=1;
+    }
+    public String getBreed(){
+        return breed;
+    }
+
+    public ArrayList getPetDetails(){
+        return personality;
+    }
+
+    public void addPetDetails(String trait){
+        personality.add(trait);
+    }
+
+    public String getBio(){
+        return bio;
+    }
+
+    public void setBio(String newBio){
+        this.bio = newBio;
+    }
+    public void markUnavailable(){
+        this.isAvailable = false;
+    }
+
 }
