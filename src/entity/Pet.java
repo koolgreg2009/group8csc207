881d6620f95c65f4f8617d98b565cbe4d739a9c9
Joyce12345678 <73724033+Joyce12345678@users.noreply.github.com>
2024-07-10 13:49:33 -0400

    Pet.java

--- src/entity/Pet.java
+++ src/entity/Pet.java
@@ -2,6 +2,9 @@ package entity;
 
 import java.util.ArrayList;
 
+/** Represents a pet.
+ * @author uwuowouwuowouwuowouwuowo
+ */
 public class Pet{
     private int petID;
     private String species;
@@ -11,6 +14,15 @@ public class Pet{
     private String bio;
     private boolean isAvailable;
 
+    /** Pet class constructor
+     * @param petID
+     * @param species
+     * @param age
+     * @param breed
+     * @param personality
+     * @param bio
+     * @param isAvailable
+     */
     public Pet(int petID, String species, int age, String breed, ArrayList personality, String bio, boolean isAvailable){
         this.petID = petID;
         this.species = species;
@@ -20,35 +32,60 @@ public class Pet{
         this.bio = bio;
         this.isAvailable = true;
     }
+    /**Gets the species of the pet
+     * @return A string of the species of the pet
+     */
     public String getSpecies(){
         return species;
     }
-
+    /**Gets the age of the pet
+     * @return An int of the pets's age
+     */
     public int getPetAge(){
         return age;
     }
+
+    /** Adds one to the pet's age after a year
+     */
     public void petGetsOlder(){
         age +=1;
     }
+
+    /**Gets the breed of the pet
+     * @return A String of the pets's breed
+     */
     public String getBreed(){
         return breed;
     }
 
+    /**Gets the personality of the pet
+     * @return A list of the pets's personality(s)
+     */
     public ArrayList getPetDetails(){
         return personality;
     }
 
+    /** Adds to the pet's personality
+     */
     public void addPetDetails(String trait){
         personality.add(trait);
     }
 
+    /**Gets the bio of the pet
+     * @return A String of the pets's biography
+     */
     public String getBio(){
         return bio;
     }
 
+    /** Adds to the pet's bio
+     */
     public void setBio(String newBio){
         this.bio = newBio;
     }
+
+    /**Set pet unavailable
+     */
     public void markUnavailable(){
         this.isAvailable = false;
     }
