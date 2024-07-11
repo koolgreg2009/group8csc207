ee0486d74a022acb1abb4d8c9ff273395a219f00
jiaro <janejiarong.li@mail.utoronto.ca>
2024-07-11 19:47:45 -0400

    Removed unnecessary Bookmark interfaces since no classes are going to extend/implement Bookmark class. Added javadocs.

--- src/entity/Bookmark.java
+++ src/entity/Bookmark.java
@@ -1,18 +1,20 @@
 package entity;
 
-import entity.User.AdopterUser;
-
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Bookmark implements BookmarkInterface {
-    /** Bookmarks that user made to save pets they are interested in adopting.
+public class Bookmark {
+    /** Class containing bookmarks that adopter user made to save pets they are interested
+     * in adopting.
     */
     private Pet pet;
     private LocalDateTime bookmarkedDate;
 
     public Bookmark(Pet petProfile, LocalDateTime time) {
+        /** Initializer for a bookmark.
+        * @param pet the pet in the listing being bookmarked.
+        * @param time the date and time the bookmark is created.
+        */
+
         this.pet = petProfile;
         this.bookmarkedDate = time;
     }
