0d58db74654ea23323151081e2b4ba5b1b8b7307
kevinhu <kaiyuan.hu@mail.utoronto.ca>
2024-07-16 01:31:14 -0400

    made some fixes to interactor and changed userfactory return type to adopteruser. do we want to do that?

--- src/entity/User/AdopterUserFactory.java
+++ src/entity/User/AdopterUserFactory.java
@@ -1,9 +1,14 @@
 package entity.User;
 
+import java.time.LocalDateTime;
+
 public class AdopterUserFactory implements UserFactory {
 
     @Override
-    public User create(String username, String password, String name, String email, String phone){
+    public AdopterUser create(String username, String password, String name, String email, String phone){
         return new AdopterUser(username, password, name, email, phone);
     }
+
+
+
 }
