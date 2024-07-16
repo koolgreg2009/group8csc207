95aff249626af737d48abce9549b56df02a19662
alambo <alexander.lambermon@mail.utoronto.ca>
2024-07-16 17:31:18 -0400

    updated bookmark javadoc

--- src/use_case/EditPreferenceInteractor.java
+++ src/use_case/EditPreferenceInteractor.java
@@ -2,6 +2,6 @@ package use_case;
 
 import data_access.FileUserDAO;
 
-public class EditPreferenceInteractor {
+public class EditPreferenceInteractor implements PreferenceInputBoundary{
 
 }
--- src/use_case/PreferenceOutputBoundary.java
+++ src/use_case/PreferenceOutputBoundary.java
@@ -1,4 +1,5 @@
 package use_case;
 
 public interface PreferenceOutputBoundary {
+    void prepareSuccessView(PreferenceOutputBoundary user);
 }
