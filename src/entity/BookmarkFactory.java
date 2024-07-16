ec92782ab8b9cd7d2c1ea6c51ebfbb3cccec67e5
jiaro <janejiarong.li@mail.utoronto.ca>
2024-07-16 11:38:11 -0400

    Added gender attribute in Pet and UserPreference. Edited parameters and Javadocs in Bookmark, BookmarkFactory, Pet, PreferenceData, and UserPreference.

--- src/entity/BookmarkFactory.java
+++ src/entity/BookmarkFactory.java
@@ -4,20 +4,19 @@ import java.time.LocalDateTime;
 /** The BookmarkFactory class is a factory responsible for the creation of Bookmark
  * entities for the adopter user.
  *
- * @author Jane Li
- * @version 1.0
- * @since 2024-07-15
+ * @version 2.0
+ * @since 2024-07-16
  *
  */
 
 public class BookmarkFactory {
 
     /** The create method creates a Bookmark.
-     * @param pet The pet that is being bookmarked.
+     * @param petID The ID of pet that is being bookmarked.
      * @param time The date and time that the bookmark is created
      * @return the newly created Bookmark.
      */
-    public Bookmark create(Pet pet, LocalDateTime time) {
-        return new Bookmark(pet, time); // return new bookmark
+    public Bookmark create(int petID, LocalDateTime time) {
+        return new Bookmark(petID, time); // return new bookmark
     }
 }
--- src/use_case/PreferenceData.java
+++ src/use_case/PreferenceData.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 /** The PreferenceData class bundles user input data for their Preference profile.
  *
- * @author Jane Li
  * @version 1.0
  * @since 2024-07-15
  */
@@ -25,9 +24,11 @@ public class PreferenceData {
      * @param minAge
      * @param maxAge
      * @param activityLevel
+     * @param gender
      */
+
     public PreferenceData(String location, List<String> species, List<String> breeds, int minAge,
-                          int maxAge, String activityLevel) {
+                          int maxAge, String activityLevel, String gender) {
         this.location = location;
         this.species = species;
         this.breeds = breeds;
@@ -37,44 +38,37 @@ public class PreferenceData {
         this.gender = gender;
     }
 
-    /**
-     * Returns the location that the user set for their search.
+    /** Gets the location that the user set for their search.
      * @return the location as a String.
      */
     String getLocation() {return location;}
 
-    /**
-     * Returns the species that the user prefers in their search.
+    /** Gets the species that the user prefers in their search.
      * @return the species as a List of Strings.
      */
     List<String> getSpecies() {return species;}
 
-    /**
-     * Returns the breeds that the user prefers in their search.
+    /** Gets the breeds that the user prefers in their search.
      * @return the breeds as a List of Strings.
      */
     List<String> getBreeds() {return breeds;}
 
-    /**
-     * Returns the minimum age of pet that the user prefers.
+    /** Gets the minimum age of pet that the user prefers.
      * @return the minimum age as an int.
      */
     int getMinAge() {return minAge;}
 
-    /**
-     * Returns the maximum age of pet that the user prefers.
+    /** Gets the maximum age of pet that the user prefers.
      * @return the maximum age as an int.
      */
     int getMaxAge() {return maxAge;}
 
-    /**
-     * Returns the activity level of pet that the user prefers.
+    /** Gets the activity level of pet that the user prefers.
      * @return the activity level as a String.
      */
     String getActivityLevel() {return activityLevel;}
 
-    /**
-     * Returns the gender of the pet that the user prefers.
+    /** Gets the gender of the pet that the user prefers.
      * @return the gender as a String.
      */
     String getGender() {return gender;}
