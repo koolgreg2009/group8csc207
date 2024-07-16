fcd60194eac74a7db57125d42c2f4c8905ab6421
alambo <alexander.lambermon@mail.utoronto.ca>
2024-07-16 17:43:29 -0400

    updated bookmark javadoc

--- src/use_case/EditPreferenceInteractor.java
+++ src/use_case/EditPreferenceInteractor.java
@@ -1,7 +1,17 @@
 package use_case;
 
 import data_access.FileUserDAO;
+import use_case.PreferenceUseCase.InitialPreferenceData;
 
 public class EditPreferenceInteractor implements PreferenceInputBoundary{
 
+    @Override
+    public void execute(InitialPreferenceData initialPreferenceData){
+        //Implement this
+        //Retrieve the user from the JSON File THROUGH DAO
+        //Then take the information from InitialPreferenceData and update the user java object
+        //Send updated user back to the DAO
+        //Then make an output data object (PreferenceOutputData) to send to presenter by evoking the presenter
+
+    }
 }
--- src/use_case/PreferenceInputBoundary.java
+++ src/use_case/PreferenceInputBoundary.java
@@ -1,4 +1,7 @@
 package use_case;
 
+import use_case.PreferenceUseCase.InitialPreferenceData;
+
 public interface PreferenceInputBoundary {
+    void execute(InitialPreferenceData initialPreferenceData);
 }
