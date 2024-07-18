741cc014ecc74872ea365e26f978e2e69ac4f945
zhongli <joyc5678@gmail.com>
2024-07-18 17:39:45 -0400

    uwu im too lazy to write the rest

--- src/use_case/Adopt.java
+++ src/use_case/Adopt.java
@@ -7,15 +7,19 @@ import entity.Pet;
 public class Adopt implements AdoptInputBoundary {
     final PetDAOInterface petDAO;
     final AdoptOutputBoundary userPresenter;
+    final UserDAOInterface userDAO;
 
-    public Adopt(PetDAOInterface petDAO, AdoptOutputBoundary userPresenter){
+    public Adopt(PetDAOInterface petDAO, AdoptOutputBoundary userPresenter, UserDAOInterface userDAO){
         this.petDAO = petDAO;
         this.userPresenter = userPresenter;
+        this.userDAO = userDAO;
     }
 
     @Override
     public void execute(AdoptInputData adoptInputData) {
         userPresenter.prepareAdopt("Pet has found a home");
         Pet uwu =  petDAO.get(adoptInputData.getPetID());
+        uwu.markUnavailable();
+        userDAO.removePetFromAllUserBookmarks(uwu.getPetID());
     }
 }
