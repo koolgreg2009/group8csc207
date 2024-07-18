c721a1ead3f827b91856c3f52b4de47d4eb9505d
kevinhu <kaiyuan.hu@mail.utoronto.ca>
2024-07-17 23:10:48 -0400

    moved and refactored preference use case files

--- src/use_case/PreferenceInputBoundary.java
+++
@@ -1,7 +0,0 @@
-package use_case;
-
-import use_case.PreferenceUseCase.InitialPreferenceData;
-
-public interface PreferenceInputBoundary {
-    void execute(InitialPreferenceData initialPreferenceData);
-}
