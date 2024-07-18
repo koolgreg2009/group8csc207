f3509124de0a3629a312fae663add098bfb7786f
alambo <alexander.lambermon@mail.utoronto.ca>
2024-07-18 18:53:17 -0400

    Done use case for Edit Preferences

--- src/use_case/PreferenceUseCase/EditPreferenceInteractor.java
+++ src/use_case/PreferenceUseCase/EditPreferenceInteractor.java
@@ -1,14 +1,42 @@
 package use_case.PreferenceUseCase;
 
-public class EditPreferenceInteractor implements PreferenceInputBoundary {
+import data_access.UserDAOInterface;
+import entity.User.AdopterUser;
+import entity.User.User;
 
+/**
+ * Interactor for use case of editing an adopter user's pet preference data.
+ */
+public class EditPreferenceInteractor implements PreferenceInputBoundary {
+    final UserDAOInterface userDataAccessObject;
+    final PreferenceOutputBoundary userPresenter;
+
+    /**
+     * Constructor for editing preferences.
+     *
+     * @param userDataAccessObject Access object for the adopter user that you are changing preferences for
+     * @param signupOutputBoundary output boundary for new preferences to be passed through and presented
+     */
+    public EditPreferenceInteractor(UserDAOInterface userDataAccessObject,
+                            PreferenceOutputBoundary signupOutputBoundary) {
+        this.userDataAccessObject = userDataAccessObject;
+        this.userPresenter = signupOutputBoundary;
+    }
+
+    /**
+     * Executes the use case to edit the adopter user's preferences.
+     *
+     * @param PreferenceData is the preference data for the adopter user which is trying to edit preferences
+     */
     @Override
     public void execute(PreferenceData PreferenceData){
-        //Implement this
-        //Retrieve the user from the JSON File THROUGH DAO
-        //Then take the information from InitialPreferenceData and update the user java object
-        //Send updated user back to the DAO
-        //Then make an output data object (PreferenceOutputData) to send to presenter by evoking the presenter
+        AdopterUser user = userDataAccessObject.get(PreferenceData.getUsername());
 
+        user.setPreferences(PreferenceData.getPreference());
+
+        userDataAccessObject.save(user);
+
+        System.out.println(user.getUsername() + " preferences have been updated");
     }
+
 }
