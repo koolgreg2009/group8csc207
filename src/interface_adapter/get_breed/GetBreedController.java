f13b1de23633d8cbf19823fce9ef1eba9d2a12d2
kevinhu <kaiyuan.hu@mail.utoronto.ca>
2024-07-20 01:59:33 -0400

    i finished one of the use cases

---
+++ src/interface_adapter/get_breed/GetBreedController.java
@@ -0,0 +1,21 @@
+package interface_adapter.get_breed;
+import java.util.Scanner;
+
+import use_case.get_breed_info.GetBreedInputBoundary;
+import use_case.get_breed_info.GetBreedInputData;
+
+public class GetBreedController {
+    private final GetBreedInputBoundary getBreedInteractor;
+
+    public GetBreedController(GetBreedInputBoundary getBreedInteractor) {
+        this.getBreedInteractor = getBreedInteractor;
+    }
+
+    public void execute(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter breed");
+        String breed = scanner.nextLine();
+        this.getBreedInteractor.execute(new GetBreedInputData(breed));
+        scanner.close();
+    }
+}
