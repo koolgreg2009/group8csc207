454f3c1da6b97c8dc99df6724dba98abfe31a445
Justin Yoon <justinyoon95s2@gmail.com>
2024-07-19 21:24:17 -0400

    More... java docs. (Used the power of chatGPT)

--- src/interface_adapter/login/LoginController.java
+++ src/interface_adapter/login/LoginController.java
@@ -3,14 +3,34 @@ package interface_adapter.login;
 import use_case.login.LoginInputData;
 import use_case.login.LoginInputBoundary;
 
+/**
+ * The LoginController class handles user login requests and interacts with the login use case.
+ * It takes user input for login and delegates the processing to the login use case interactor.
+ *
+ * @version 1.0
+ * @since 2024-07-19
+ */
 public class LoginController {
 
+    /** The interactor for processing login input data. */
     final LoginInputBoundary loginUseCaseInteractor;
+
+    /**
+     * Constructs a new LoginController with the specified login use case interactor.
+     *
+     * @param loginUseCaseInteractor
+     */
     public LoginController(LoginInputBoundary loginUseCaseInteractor) {
         this.loginUseCaseInteractor = loginUseCaseInteractor;
     }
 
-
+    /**
+     * Executes the login process with the provided username and password.
+     * Creates a LoginInputData object with the provided credentials and invokes the login use case interactor.
+     *
+     * @param username
+     * @param password
+     */
     public void execute(String username, String password) {
         LoginInputData loginInputData = new LoginInputData(
                 username, password);
