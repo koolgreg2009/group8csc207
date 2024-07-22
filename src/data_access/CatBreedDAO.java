58ee38dace333e5928539a511b9010ef7547e6ba
kevinhu <kaiyuan.hu@mail.utoronto.ca>
2024-07-22 00:46:13 -0400

    added javadoc

--- src/data_access/CatBreedDAO.java
+++ src/data_access/CatBreedDAO.java
@@ -5,11 +5,23 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
-public class CatBreedDAO implements CatDAOInterface{
+
+/**
+ * The CatBreedDAO class is responsible for fetching breed information from TheCatAPI.
+ * It implements the CatDAOInterface interface and uses the OkHttpClient library to
+ * perform HTTP requests.
+ */
+public class CatBreedDAO implements CatDAOInterface {
     private static final String API_KEY = "live_DUQgLUE2gN2cW7JxlvdupPwS1n2CyUFee5yfvYXnyUps7D6CEF9yjk6bCHdwqGiY";
     private static final String API_URL = "https://api.thecatapi.com/v1/breeds/search";
     private final OkHttpClient client = new OkHttpClient();
 
+    /**
+     * Fetches breed information for a given breed name from TheCatAPI.
+     *
+     * @param breedName The name of the breed to fetch information for.
+     * @return A JSON string containing the breed information, or null if an error occurs.
+     */
     @Override
     public String getBreedInformation(String breedName) {
         String url = API_URL + "?q=" + breedName + "&attach_image=1";
@@ -22,7 +34,7 @@ public class CatBreedDAO implements CatDAOInterface{
         try (Response response = client.newCall(request).execute()) {
             if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
-            // return the json response as a string
+            // Return the JSON response as a string
             return response.body().string();
         } catch (IOException e) {
             e.printStackTrace();
