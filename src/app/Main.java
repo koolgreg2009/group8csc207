e4c7178552d1e0e4b3ae835aef96d994757083a3
kevinhu <kaiyuan.hu@mail.utoronto.ca>
2024-08-03 19:04:44 -0400

    experimenting with pet listing

--- src/app/Main.java
+++ src/app/Main.java
@@ -41,13 +41,13 @@ public class Main {
         // The main application window.
         JFrame application = new JFrame("Login Page");
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
+        application.setResizable(false);
+        application.setSize(800, 500);
         CardLayout cardLayout = new CardLayout();
 
         // The various View objects. Only one view is visible at a time.
         JPanel views = new JPanel(cardLayout);
         application.add(views);
-
         // This keeps track of and manages which view is currently showing.
         ViewManagerModel viewManagerModel = new ViewManagerModel();
         new ViewManager(views, cardLayout, viewManagerModel);
@@ -98,7 +98,8 @@ public class Main {
         viewManagerModel.setActiveView(loginView.viewName);
         viewManagerModel.firePropertyChanged();
 
-        application.pack();
+        //application.pack();
+        application.setLocationRelativeTo(null);
         application.setVisible(true);
     }
 }
