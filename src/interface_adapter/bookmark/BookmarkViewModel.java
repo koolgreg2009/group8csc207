22db718a78c6de68ba53a4e5c269c4de7c00206a
jiaro <janejiarong.li@mail.utoronto.ca>
2024-08-06 22:26:45 -0400

    Finished BookmarkViewModel.

--- src/interface_adapter/bookmark/BookmarkViewModel.java
+++ src/interface_adapter/bookmark/BookmarkViewModel.java
@@ -1,12 +1,18 @@
 package interface_adapter.bookmark;
 
+import dto.PetDTO;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.ViewModel;
 import view.ViewManager;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.time.LocalDateTime;
+import java.util.List;
 
+/**
+ * ViewModel for managing bookmarks.
+ */
 public class BookmarkViewModel extends ViewModel {
     public final String PREF_BUTTON_LABEL = "My Preferences";
     public final String HOME_BUTTON_LABEL = "Home Page";
@@ -15,35 +21,96 @@ public class BookmarkViewModel extends ViewModel {
 
     private BookmarkState bookmarkState = new BookmarkState();
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
-    private final ViewManagerModel viewManagerModel;
 
-    public BookmarkViewModel(ViewManagerModel viewManagerModel) {
+    /**
+     * Constructor for BookmarkViewModel.
+     */
+    public BookmarkViewModel() {
         super("bookmark");
-        this.viewManagerModel = viewManagerModel;
     }
 
+    /**
+     * Sets a new BookmarkState.
+     *
+     * @param newState the new BookmarkState
+     */
     public void setState(BookmarkState newState){
         this.bookmarkState = newState;
     }
 
+    /**
+     * Notifies listeners of state changes.
+     */
     @Override
     public void firePropertyChanged() {
-        support.firePropertyChange("state", null, this.bookmarkState);
+        support.firePropertyChange("Bookmark State", null,this.bookmarkState);
     }
 
+    /**
+     * Adds a PropertyChangeListener.
+     *
+     * @param listener the PropertyChangeListener to add
+     */
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
 
-    public BookmarkState getState(){
-        return bookmarkState;
+    /**
+     * Removes a PropertyChangeListener.
+     *
+     * @param listener the PropertyChangeListener to remove
+     */
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        support.removePropertyChangeListener(listener);
     }
 
-    // Want to provide methods that cater to the specific needs of the view. If the view is
-    // to display pet gender, age, name, etc., have methods to reflect that.
+    /**
+     * Retrieves all bookmarked pets.
+     *
+     * @return a list of all bookmarked pets
+     */
+    public List<PetDTO> getAllBookmarks(){
+        return bookmarkState.getAllBookmarks();
+    }
 
-    // Need methods to add/remove bookmark via accessing the state. return bookmarkState.getAllBookmarks();
+    /**
+     * Adds a new pet to the list of bookmarked pets if it is not already present.
+     *
+     * @param pet the PetDTO to add
+     */
+    public void addBookmark(PetDTO pet){
+        bookmarkState.addBookmark(pet);
+        firePropertyChanged();
+    }
 
-    // In UI, use methods to get pet details (check with Kevin what info are we putting on).
+    /**
+     * Removes a pet from the list of bookmarked pets.
+     *
+     * @param pet the PetDTO to remove
+     */
+    public void removeBookmark(PetDTO pet){
+        bookmarkState.removeBookmark(pet.getPetID());
+        firePropertyChanged();
+    }
+
+    /**
+     * Checks if a pet is already bookmarked.
+     *
+     * @param pet the PetDTO to check
+     * @return true if the pet is bookmarked, false otherwise
+     */
+    public boolean isBookmarked(PetDTO pet){
+        return bookmarkState.isBookmarked(pet);
+    }
+
+    /**
+     * Gets the current date and time when the pet was bookmarked.
+     *
+     * @param pet the PetDTO to get the bookmark time for
+     * @return the current date and time as a LocalDateTime instance
+     */
+    public LocalDateTime getBookmarkTime(PetDTO pet){
+        return bookmarkState.getBookmarkTime(pet);
+    }
 }
