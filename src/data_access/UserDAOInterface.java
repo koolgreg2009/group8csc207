2616edf73cd5efdd9d6d26cc4e587eec01a77949
Justin Yoon <justinyoon95s2@gmail.com>
2024-08-08 00:07:20 -0400

    Removed UserSignupDAInterface.java

--- src/data_access/UserDAOInterface.java
+++ src/data_access/UserDAOInterface.java
@@ -4,12 +4,32 @@ import entity.user.User;
 import java.util.List;
 
 public interface UserDAOInterface {
+
+	/**
+	 * Saves the specified user to the data source.
+	 *
+	 * @param user The user to be saved.
+	 */
     void save(User user);
+
+	/**
+	 * Checks if a user with the specified identifier (username) already exists in the data source.
+	 *
+	 * @param identifier
+	 * @return true if a user with the specified username exists, false otherwise.
+	 */
     boolean existsByName(String identifier);
+
 	boolean existsByEmail(String email);
+
 	boolean existsByPhone(String phone);
+
 	String clearUsers();
+
 	User get(String username);
+
 	List<String> removePetFromAllUserBookmarks(int petID);
+
 	boolean userHasBookmark(String username, int petID);
+
 }
