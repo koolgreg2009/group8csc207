d4373a2dfab1127f82a35685f940de87e81e4f4c
Justin Yoon <justinyoon95s2@gmail.com>
2024-08-08 00:04:26 -0400

    UserDAOinterface update

--- src/data_access/UserDAOInterface.java
+++ src/data_access/UserDAOInterface.java
@@ -6,6 +6,8 @@ import java.util.List;
 public interface UserDAOInterface {
     void save(User user);
     boolean existsByName(String identifier);
+	boolean existsByEmail(String email);
+	boolean existsByPhone(String phone);
 	String clearUsers();
 	User get(String username);
 	List<String> removePetFromAllUserBookmarks(int petID);
