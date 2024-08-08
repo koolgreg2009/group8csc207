89ce8b27a324edbaefdde3b3f0bfe8a0668c02d3
Justin Yoon <justinyoon95s2@gmail.com>
2024-08-08 00:57:56 -0400

    UserDAOInterface Updated

--- src/data_access/UserDAOInterface.java
+++ src/data_access/UserDAOInterface.java
@@ -4,10 +4,32 @@ import entity.user.User;
 import java.util.List;
 
 public interface UserDAOInterface {
-    void save(User user);
-    boolean existsByName(String identifier);
+
+	/**
+	 * Saves the specified user to the data source.
+	 *
+	 * @param user The user to be saved.
+	 */
+	void save(User user);
+
+	/**
+	 * Checks if a user with the specified identifier (username) already exists in the data source.
+	 *
+	 * @param identifier
+	 * @return true if a user with the specified username exists, false otherwise.
+	 */
+	boolean existsByName(String identifier);
+
+	boolean existsByEmail(String email);
+
+	boolean existsByPhone(String phone);
+
 	String clearUsers();
+
 	User get(String username);
+
 	List<String> removePetFromAllUserBookmarks(int petID);
+
 	boolean userHasBookmark(String username, int petID);
+
 }
